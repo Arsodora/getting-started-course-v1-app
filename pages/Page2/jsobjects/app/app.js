f19ec80773a64b4828e8a208f46db6e9a72e6b62
Dominic Arsodora <inscription@arsodora.com>
2025-02-19 03:00:38 +0000

    First Commit

---
+++ pages/Page2/jsobjects/app/app.js
@@ -0,0 +1,27 @@
+export default {
+ init(){
+	 data.todos = appsmith.store.todos || data.dummyTodos;
+ },
+	
+	updateTodo(id,update)
+	{
+		data.todos[id] = {...data.todos[id],...update};
+		storeValue('todos',data.todos);
+	},
+	
+	addTodo(title){
+		this.updateTodo(data.todos.length, {title, complete:false});
+	},
+	
+	deleteTodo(id){
+		data.todos.splice(id,1);
+		storeValue('todos',data.todos);
+	},
+	
+	editTodo(id,title){
+		this.updateTodo(id,{title});
+		data.activeTodo = undefined;
+	}
+	
+	
+}
---
+++ pages/Page2/jsobjects/data/data.js
@@ -0,0 +1,10 @@
+export default {
+	todos: undefined,
+	activeTodo: undefined,
+	dummyTodos : [
+		{title: 'use the input to add a todo', completed:true},
+		{title: 'LINE 2', completed:false},
+		{title: 'Line 3', completed:false},
+		{title: 'Line 4', completed:false}
+		]
+}
